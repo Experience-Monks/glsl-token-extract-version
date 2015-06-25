286f9ad76e3136d7bdd22f586b3f61f2d416f2f8
mattdesl <dave.des@gmail.com>
2015-06-25 13:18:43 -0400

    split off to reuse elsewhere

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+var regex = require('glsl-version-regex')
+
 module.exports = function glslExtractVersion (tokens) {
   for (var i = 0; i < tokens.length; i++) {
     var token = tokens[i]
     if (token.type === 'preprocessor') {
-      var regex = /^\s*\#version\s+([0-9]+(\s+es)?)\s*/g
       var match = regex.exec(token.data)
       if (match) {
         return match[1].replace(/\s\s+/g, ' ')
